94fa4d49c2ed4769363b8581a12456bae18894ee
Frankincense <frankincensewelsey@gmail.com>
2025-05-22 11:22:24 +0300

    Alembic not understood

---
+++ app/migrations/versions/1613fdf671bc_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 1613fdf671bc
+Revises: 
+Create Date: 2025-05-22 10:59:47.414118
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1613fdf671bc'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
